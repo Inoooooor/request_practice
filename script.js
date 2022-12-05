de61f788b13333a32964d271c175404799abdf49
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2022-12-05 19:35:36 +0300

    Added_request_print

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
+
+const hackButton = document.getElementById('hacking_button');
+hackButton.addEventListener('click', () => {
+  console.log(randObj);
+});
+
+
 fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(json => {
-    console.log(Object.keys(json[0])[0])
-  });
+.then(response => response.json())
+.then(json => {
+    let randObj = Math.floor(Math.random() * json.length);
+    console.log(randObj);
+    for (let key in json[0]) {
+      if (key == "address") {
+        for (let addressKey in json[0].address) {
+          if (addressKey != "geo") {
+            console.log(addressKey + " = " + json[0].address[addressKey]);
+            const pElement = document.createElement('p');
+            pElement.classList = "hack_text";
+            pElement.innerHTML = addressKey + " = " + json[0].address[addressKey];
+            hackInfoDiv.append(pElement);        
+          }
+        }
+        continue;
+      } else if (key == "company" || key == "id") {
+        continue;
+      }
+    console.log(key + " = " + json[0][key]);
+    const pElement = document.createElement('p');
+    pElement.classList = "hack_text";
+    pElement.innerHTML = key + " = " + json[0][key];
+    hackInfoDiv.append(pElement);
+  }
+});
 
 
 
@@ -16,7 +45,6 @@ imgDiv.append(hackerImg);
 
 
 const hackInfoDiv = document.getElementById('hacked_info');
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(response => response.json())
-.then(json => {hackInfoDiv.innerHTML = (Object.keys(json[0])[0]) + " = "});
-
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(response => response.json())
+// .then(json => console.log(json[0]));
